f9a4a7ceb5b825f2e72cb98b0fe7eb57446db12f
= <=>
2019-10-30 11:52:04 +0700

    hello

---
+++ src/Dang_Ba_Hau/Main.java
@@ -0,0 +1,14 @@
+package Dang_Ba_Hau;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Enter your name");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Hello "+name);
+    }
+}
